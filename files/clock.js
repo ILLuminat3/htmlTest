b81c2c08e6b73ef20d2ad3a276b24cf909ec06b3
ILLuminat3 <60465703+ILLuminat3@users.noreply.github.com>
2022-05-07 01:45:11 +0100

    Add files via upload

---
+++ files/clock.js
@@ -0,0 +1,12 @@
+var clock = document.getElementById("clock");
+
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  clock.textContent = 
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
+
+setInterval(time, 1000);
